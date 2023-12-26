03c12699a3ecc04c128b089e0e3df19f10934d6a
0xff3232 <zanechocolinked@gmail.com>
2023-12-26 11:47:13 +1100

    markdown parsing to html working

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"github.com/0xff3232/blog/handler"
-	"github.com/0xff3232/blog/model"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	app := echo.New()
 
-	postModel := model.PostModel{}
-
-	postHandler := handler.NewPostHanlder(postModel)
 	userHandler := handler.UserHandler{}
 
 	app.GET("/home", userHandler.HandleUserShow)
 
-	app.GET("/test", func(c echo.Context) error {
-		return postHandler.HandleCreatePost(c, "test.md")
-	})
+	blogHandler := handler.NewBlogHandler()
+
+	app.GET("/article/:path", blogHandler.HandleBlogShow)
 
 	app.Start(":8080")
 
